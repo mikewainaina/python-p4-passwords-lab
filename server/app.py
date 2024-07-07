77789a3f3b9bac1761739f1ec7df0a5ec71a1d9f
mikewainaina <michael.wainaina@student.moringaschool.com>
2024-07-07 17:54:56 +0300

    labs done

--- server/app.py
+++ server/app.py
@@ -28,16 +28,39 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
+    def get(self):
+        if 'user_id' in session and session['user_id'] is not None:
+            user_id = int(session['user_id'])
+            user = db.session.get(User, user_id)
+            if user:
+                return user.to_dict(), 200
+        return {'': ''}, 204
     pass
 
 class Login(Resource):
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            session['page_views'] = 0
+            return user.to_dict(), 200
+        else:
+            return {}, 401
     pass
 
 class Logout(Resource):
+    def delete(self):
+        session['page_views'] = None
+        session['user_id']=  None
+        return {}, 204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
